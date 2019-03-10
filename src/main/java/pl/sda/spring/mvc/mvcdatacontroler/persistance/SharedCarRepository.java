3a4cae4ee672b78ae2927a6486c785862ffd9dc8
Marcin Iskra <dunravel@gmail.com>
2019-03-10 11:01:29 +0100

    Added SharedCar repository

---
+++ src/main/java/pl/sda/spring/mvc/mvcdatacontroler/persistance/SharedCarRepository.java
@@ -0,0 +1,6 @@
+package pl.sda.spring.mvc.mvcdatacontroler.persistance;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SharedCarRepository extends CrudRepository<SharedCar, Long> {
+}
